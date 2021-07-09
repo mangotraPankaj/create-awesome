c1ab3286d0b89fc7dfa19628450054acdf05462d
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-09 13:26:54 +0530

    Saved command requests cache deletion. Deleted the test plan

--- EDNLearnMacTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EDNLearnMacTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -5,14 +5,26 @@
 //  Created by Pankaj Mangotra on 09/07/21.
 //
 
+import EDNLearnMac
 import XCTest
 
 class LoadFeedLoader {
-    init(store: FeedStore) {}
+    private let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCacheFeed()
+    }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCacheFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -20,5 +32,24 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LoadFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LoadFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
 }
