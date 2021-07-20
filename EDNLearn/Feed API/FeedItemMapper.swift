40357c855ebd214e82ec948454ee8eb579f45256
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-07-20 12:15:43 +0530

    Moved RemoteFeedItem to its own file.

--- EDNLearn/Feed API/FeedItemMapper.swift
+++ EDNLearn/Feed API/FeedItemMapper.swift
@@ -7,15 +7,6 @@
 
 import Foundation
 
-
-internal struct RemoteFeedItem: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-
-}
-
 internal enum FeedItemMapper {
     private struct Root: Decodable {
         var items: [RemoteFeedItem]
---
+++ EDNLearn/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedItem.swift
+//  EDNLearn
+//
+//  Created by Pankaj Mangotra on 20/07/21.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+
+}
