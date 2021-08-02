23d78028b352a09d5f064576138cfc0acaf4928d
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-02 12:20:12 +0530

    Validate cache command doesn't delete cache when it is less than 7 days old.

--- EDNLearnMacTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EDNLearnMacTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -80,7 +80,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
 
-    func test_load_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+    func test_load_hasNoSideEffectsOnLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let lessThanSevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
--- EDNLearnMacTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EDNLearnMacTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -21,10 +21,23 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
 
-    func test_validate_doesNotDeletesCacheOnEmptyCache() {
+    func test_validateCache_doesNotDeletesCacheOnEmptyCache() {
         let (sut, store) = makeSUT()
         sut.validateCache()
         store.completeRetrievalwithEmptyCache()
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+
+    func test_validateCache_doesNotDeletesCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        sut.validateCache()
+
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimeStamp)
+
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
 
@@ -41,5 +54,29 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
 
     private func anyError() -> NSError {
         return NSError(domain: "any error", code: 1)
+    }
+
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueImage(), uniqueImage()]
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        return (models, local)
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
