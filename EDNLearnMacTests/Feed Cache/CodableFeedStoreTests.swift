418bee5596aaf92eead296729d0d28d32d38fe48
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-11 12:22:40 +0530

    Retrieving after inserting to empty cache returns inserted values.

--- EDNLearnMacTests/Feed Cache/CodableFeedStoreTests.swift
+++ EDNLearnMacTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,12 +9,49 @@ import EDNLearnMac
 import XCTest
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+    }
+
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
 class CodableFeedStoreTests: XCTestCase {
+    
+    /// Teardown and setup methods are written so that the residual artifacts which are written while inserting gets cleaned up otherwise test_retrieve_deliversEmptyOnEmptyCache fails since there would be data present in the cache.
+    
+    ///Teardown method is not called if the execution stops midway while test is being run. For e.g. - If you put a breakpoint at line 31 and when breakpoint hits and execution is stopped, insertion would have happened but teardown would not be called.
+    /// Hence we have to clear the artifacts both in teardown and setup which are called after and at start of the test respectively.
+
+    override func tearDown() {
+        super.tearDown()
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+
+    override func setUp() {
+        super.setUp()
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
         let exp = expectation(description: "Wait for cache retrieval")
@@ -42,7 +79,33 @@ class CodableFeedStoreTests: XCTestCase {
                     break
 
                 default:
-                    XCTFail("Expected retrieving twice from empty cache to deliver emoty results, got \(firstResult) and \(secondResult) instead")
+                    XCTFail("Expected retrieving twice from empty cache to deliver empty results, got \(firstResult) and \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+
+        let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected Feed to be inserted successfully")
+
+            sut.retrieve { retrievedResult in
+
+                switch retrievedResult {
+                case let .found(retrievedFeed, retrievedTimeStamp):
+                    XCTAssertEqual(retrievedFeed, feed)
+                    XCTAssertEqual(retrievedTimeStamp, timestamp)
+
+                default:
+                    XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrievedResult)instead")
                 }
                 exp.fulfill()
             }
