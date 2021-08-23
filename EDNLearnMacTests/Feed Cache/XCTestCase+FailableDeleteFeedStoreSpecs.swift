98df402d3eeb9655b19cc84eadbce04271549f9b
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-23 12:49:06 +0530

    Created reusable FailiableDeleteFeedStoreSpecs file.

---
+++ EDNLearnMacTests/Feed Cache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,22 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  EDNLearnMacTests
+//
+//  Created by Pankaj Mangotra on 23/08/21.
+//
+
+import EDNLearnMac
+import XCTest
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
