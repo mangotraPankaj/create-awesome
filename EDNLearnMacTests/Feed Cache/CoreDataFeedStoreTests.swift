0413e50d05652b12f20c14ff4b6a81aa82c1a3a5
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-23 13:38:32 +0530

    Added initial coreDataFeedStoreTest class

---
+++ EDNLearnMacTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,61 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EDNLearnMacTests
+//
+//  Created by Pankaj Mangotra on 23/08/21.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+
+    
+}
