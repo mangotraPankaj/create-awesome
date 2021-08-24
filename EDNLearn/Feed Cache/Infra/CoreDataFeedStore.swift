57f1dde40136af99580b6d69b5559dc083b54a2b
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-24 10:31:03 +0530

    Made changes for fixing the crash by setting correct targets for CoreData model

---
+++ EDNLearn/Feed Cache/Infra/CoreDataFeedStore.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataFeedStore.swift
+//  EDNLearn
+//
+//  Created by Pankaj Mangotra on 24/08/21.
+//
+
+import CoreData
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+    private let container: NSPersistentContainer
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
+
+    public func deleteCacheFeed(completion _: @escaping DeletionCompletion) {}
+
+    public func insert(_: [LocalFeedImage], timestamp _: Date, completion _: @escaping InsertionCompletion) {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) { completion(.empty) }
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date?
+    @NSManaged var feed: NSOrderedSet?
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID?
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL?
+    @NSManaged var cache: ManagedCache?
+}
+
+private extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle.url(forResource: name, withExtension: "momd").flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
