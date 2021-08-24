af3ddb50ce6a3b5c13377474bff2533c78d75992
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-24 09:59:59 +0530

    Added ManagedCache and ManagedFeedImage model representations.

--- EDNLearn/Feed Cache/CoreDataFeedStore.swift
+++ EDNLearn/Feed Cache/CoreDataFeedStore.swift
@@ -5,13 +5,28 @@
 //  Created by Pankaj Mangotra on 24/08/21.
 //
 
+import CoreData
 import Foundation
 
 public class CoreDataFeedStore: FeedStore {
     public init() {}
+
     public func deleteCacheFeed(completion _: @escaping DeletionCompletion) {}
 
     public func insert(_: [LocalFeedImage], timestamp _: Date, completion _: @escaping InsertionCompletion) {}
 
     public func retrieve(completion: @escaping RetrievalCompletion) { completion(.empty) }
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date?
+    @NSManaged var feed: NSOrderedSet?
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID?
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL?
+    @NSManaged var cache: ManagedCache?
 }
