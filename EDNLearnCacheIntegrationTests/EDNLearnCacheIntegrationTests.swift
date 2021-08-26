d05136b140d0ba0a5e1658c4e2830a08dcbaff19
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-26 12:55:02 +0530

    Add 'EDNLearnCacheIntegrationTests' to separate the potentially slower cache integration tests from the faster unit/isolated tests.

---
+++ EDNLearnCacheIntegrationTests/EDNLearnCacheIntegrationTests.swift
@@ -0,0 +1,10 @@
+//
+//  EDNLearnCacheIntegrationTests.swift
+//  EDNLearnCacheIntegrationTests
+//
+//  Created by Pankaj Mangotra on 26/08/21.
+//
+
+import XCTest
+
+class EDNLearnCacheIntegrationTests: XCTestCase {}
