30c181d51bc3f0d0c86d5770f658cc0433f2c176
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-08-27 11:30:04 +0530

    'LocalFeedLoader' in integration with 'CoreDataFeedStore' overrides items saved by separate instances, proving we are correctly managing the data models on disk.

--- EDNLearnCacheIntegrationTests/EDNLearnCacheIntegrationTests.swift
+++ EDNLearnCacheIntegrationTests/EDNLearnCacheIntegrationTests.swift
@@ -39,6 +39,29 @@ class EDNLearnCacheIntegrationTests: XCTestCase {
         expect(sutToPerformLoad, toLoad: feed)
     }
 
+    func test_save_overridesItemsSavedOnASeparateInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformSecondSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
+
+        let saveExp1 = expectation(description: "Wait for the save completion")
+        sutToPerformFirstSave.save(firstFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp1.fulfill()
+        }
+        wait(for: [saveExp1], timeout: 1.0)
+
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformSecondSave.save(latestFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 1.0)
+        expect(sutToPerformLoad, toLoad: latestFeed)
+    }
+
     // MARK: Helpers
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
