0c315876b18518833881d4df06d4ab224ff6b3d6
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-09-06 12:45:49 +0530

    Add feedView controller rendering 5 times

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Pankaj Mangotra on 06/09/21.
 //
 
-import Foundation
+import UIKit
+
+final class FeedViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+    }
+}
