37e8f7b754026e72f7db212cedcc143a995796fd
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-11-09 13:35:00 +0530

    Moved the files in the test target.

---
+++ EssentialFeediOSTests/Feed Presentation/FeedLocalizationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLocalizationTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Pankaj Mangotra on 09/11/21.
+//
+
+@testable import EssentialFeediOS
+import XCTest
+
+final class FeedLocalizationTests: XCTestCase {
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {}
+}
