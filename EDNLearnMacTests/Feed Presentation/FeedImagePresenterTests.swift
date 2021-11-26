7fd518bcbdcf9adb60e3e2c14d6d936e7c981a4e
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-11-26 12:48:02 +0530

    Add 'fail' imageTransformation helper to clarify intent in test setup

--- EDNLearnMacTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EDNLearnMacTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -65,7 +65,7 @@ class FeedImagePresenterTests: XCTestCase {
     }
 
     func test_didStartLoadingImageData_displaysRetryOnFailedImageTransformation() {
-        let (sut, view) = makeSUT(imageTransformer: { _ in nil })
+        let (sut, view) = makeSUT(imageTransformer: fail)
         let data = Data()
         let image = uniqueImage()
         sut.didFinishLoadingImageData(with: data, for: image)
@@ -78,7 +78,7 @@ class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(message?.shouldRetry, true)
         XCTAssertNil(message?.image)
     }
-
+    
     // MARK: - Helpers
 
     private func makeSUT(
@@ -91,6 +91,10 @@ class FeedImagePresenterTests: XCTestCase {
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
+    }
+    
+    private var fail:(Data) -> Any? {
+        return {_ in nil}
     }
 
     private class ViewSpy: FeedImageView {
