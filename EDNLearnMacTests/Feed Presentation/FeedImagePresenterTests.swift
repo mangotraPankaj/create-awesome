5ad75d8f1b72334d1f28027b432c32eb464611a3
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-11-26 13:21:12 +0530

    'FeedImagePresenter' displays retry on 'didFinishLoadingImageDataWtithError'

--- EDNLearnMacTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EDNLearnMacTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -35,7 +35,21 @@ final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == I
     }
 
     func didStartLoadingImageData(for model: FeedImage) {
-        view.display(FeedImageViewModel(description: model.description, location: model.location, image: nil, isLoading: true, shouldRetry: false))
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false))
+    }
+
+    func didFinishLoadingImageData(with _: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true))
     }
 
     func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
@@ -101,6 +115,21 @@ class FeedImagePresenterTests: XCTestCase {
         XCTAssertEqual(message?.isLoading, false)
         XCTAssertEqual(message?.shouldRetry, false)
         XCTAssertEqual(message?.image, transformedData)
+    }
+
+    func test_didFinishLoadingImageDataWithError_displaysRetryOn() {
+        let image = uniqueImage()
+        let (sut, view) = makeSUT()
+
+        sut.didFinishLoadingImageData(with: anyError(), for: image)
+
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.description, image.description)
+        XCTAssertEqual(message?.location, image.location)
+        XCTAssertEqual(message?.isLoading, false)
+        XCTAssertEqual(message?.shouldRetry, true)
+        XCTAssertNil(message?.image)
     }
 
     // MARK: - Helpers
