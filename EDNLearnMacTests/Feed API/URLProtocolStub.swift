89a0fb59d2d00f90966061c013967df2cd65b6f9
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-02 12:29:15 +0530

    Synchronise access to global 'URLProtocolStub.stub' with private queue to prevent data races

--- EDNLearnMacTests/Feed API/URLProtocolStub.swift
+++ EDNLearnMacTests/Feed API/URLProtocolStub.swift
@@ -8,17 +8,23 @@
 import Foundation
 
 class URLProtocolStub: URLProtocol {
-    private static var stub: Stub?
-    private static var requestObserver: ((URLRequest) -> Void)?
-
     private struct Stub {
         let data: Data?
         let response: URLResponse?
         let error: Error?
+        let requestObserver: ((URLRequest) -> Void)?
     }
 
+    private static var _stub: Stub?
+    private static var stub: Stub? {
+        get { return queue.sync { _stub }}
+        set { queue.sync { _stub = newValue }}
+    }
+
+    private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
+
     static func stub(data: Data?, response: URLResponse?, error: Error?) {
-        stub = Stub(data: data, response: response, error: error)
+        stub = Stub(data: data, response: response, error: error, requestObserver: nil)
     }
 
     static func startInterceptingRequest() {
@@ -28,11 +34,10 @@ class URLProtocolStub: URLProtocol {
     static func stopInterceptingRequest() {
         URLProtocol.unregisterClass(URLProtocolStub.self)
         stub = nil
-        requestObserver = nil
     }
 
     static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-        requestObserver = observer
+        stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
     }
 
     override class func canInit(with _: URLRequest) -> Bool {
@@ -44,11 +49,6 @@ class URLProtocolStub: URLProtocol {
     }
 
     override func startLoading() {
-        if let requestObserver = URLProtocolStub.requestObserver {
-            client?.urlProtocolDidFinishLoading(self)
-            return requestObserver(request)
-        }
-
         guard let stub = URLProtocolStub.stub else { return }
 
         if let data = stub.data {
@@ -64,6 +64,7 @@ class URLProtocolStub: URLProtocol {
         } else {
             client?.urlProtocolDidFinishLoading(self)
         }
+        stub.requestObserver?(request)
     }
 
     override func stopLoading() {}
