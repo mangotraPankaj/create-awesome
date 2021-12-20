46a4868fa4c753e5a24a87bf9f509e1346a5e662
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-20 12:17:32 +0530

    'CoreDataFeedImageDataStore.retrieveImageData' delivers 'image data not found' when store is not empty but there's no image with matching url

--- EDNLearnMacTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EDNLearnMacTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -23,7 +23,41 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: anyURL())
     }
 
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let url = URL(string: "https://a-url.com")!
+        let nonMatchingURL = URL(string: "https://another-url.com")!
+
+        insert(anyData(), for: url, into: sut)
+
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
+    }
+
     // MARK: Helpers
+
+    private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "wait for cache insertion")
+        let image = localImage(url: url)
+        sut.insert([image], timestamp: Date()) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail("Failed to save \(image) with error:\(error)", file: file, line: line)
+
+            case .success:
+                sut.insert(data, for: url) { result in
+                    if case let Result.failure(error) = result {
+                        XCTFail("failed to insert \(data) with error: \(error)", file: file, line: line)
+                    }
+                }
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any", url: url)
+    }
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
         let storeURL = URL(fileURLWithPath: "/dev/null")
