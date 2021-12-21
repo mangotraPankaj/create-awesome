ee2821c6a908c3fe62ad23d5b954a4efa8c148c8
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-21 16:46:52 +0530

    'LocalFeedLoader' in integration with the 'CoreDataFeedStore' deletes items saved in a distant past by separate instances when validating cache, proving we correctly manage the data models on disk.

--- EDNLearnCacheIntegrationTests/EDNLearnCacheIntegrationTests.swift
+++ EDNLearnCacheIntegrationTests/EDNLearnCacheIntegrationTests.swift
@@ -88,6 +88,17 @@ class EDNLearnCacheIntegrationTests: XCTestCase {
         expect(feedLoaderToPerformSave, toLoad: feed)
     }
 
+    func test_validateFeedCache_deletesFeedSavedInDistantPast() {
+        let feedLoaderToPerformSave = makeFeedLoader(currentDate: .distantPast)
+        let feedLoaderToPerformValidation = makeFeedLoader(currentDate: Date())
+        let feed = uniqueImageFeed().models
+
+        save(feed, with: feedLoaderToPerformSave)
+        validateCache(with: feedLoaderToPerformValidation)
+
+        expect(feedLoaderToPerformSave, toLoad: [])
+    }
+
     // MARK: Helpers
 
     private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
@@ -101,11 +112,11 @@ class EDNLearnCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-    private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+    private func makeFeedLoader(currentDate: Date = Date(), file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         // let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = testSpecificStoreURL()
         let store = try! CoreDataFeedStore(storeURL: storeURL)
-        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        let sut = LocalFeedLoader(store: store, currentDate: { currentDate })
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
