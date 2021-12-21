781c604acd75d8d30c47a3cfd2fc72dfe15ccf43
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-21 11:55:12 +0530

    Add test to guarantee that 'CoreDataFeedStore' side effects run serially to avoid any unexpected behaviour

--- EDNLearnMacTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EDNLearnMacTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -48,6 +48,24 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: url)
     }
 
+    func test_sideEffects_runSerially() {
+        let sut = makeSUT()
+        let url = anyURL()
+
+        let op1 = expectation(description: "operation 1")
+        sut.insert([localImage(url: url)], timestamp: Date()) { _ in
+            op1.fulfill()
+        }
+
+        let op2 = expectation(description: "operation 2")
+        sut.insert(anyData(), for: url) { _ in op2.fulfill() }
+
+        let op3 = expectation(description: "operation 3")
+        sut.insert(anyData(), for: url) { _ in op3.fulfill() }
+
+        wait(for: [op1, op2, op3], timeout: 5.0, enforceOrder: true)
+    }
+
     // MARK: Helpers
 
     private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
@@ -57,15 +75,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
             switch result {
             case let .failure(error):
                 XCTFail("Failed to save \(image) with error:\(error)", file: file, line: line)
-
+                exp.fulfill()
+                    
             case .success:
                 sut.insert(data, for: url) { result in
                     if case let Result.failure(error) = result {
                         XCTFail("failed to insert \(data) with error: \(error)", file: file, line: line)
                     }
+                    exp.fulfill()
                 }
             }
-            exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
