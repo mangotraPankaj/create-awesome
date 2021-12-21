41d2c4e9623aea815a2ecff021253dfd92f07630
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-21 17:27:06 +0530

    'LocalFeedStore.validateCache' succeeds on empty cache

--- EDNLearnMacTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EDNLearnMacTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -81,6 +81,14 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
         })
     }
 
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrievalwithEmptyCache()
+        })
+    }
+
     func test_validateCache_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
