4fc342af7b95a1d850aa466dc3c8dcbb10eb6a88
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-12-27 12:58:38 +0530

    Made a demo composer.-IMAGES NOT LOADING DUE TO INCORRECT IMAGE URL
    Deleted the boilerplate tests.

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -17,6 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5d1c78f21e661a0001ce7cfd/1562147059075/feed-case-study-v1-api-feed.json")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let feedLoader = RemoteFeedLoader(url: url, client: client)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+        let feedViewController = FeedUIComposer.feedComposedWith(feedLoader: feedLoader, imageLoader: imageLoader)
+
+        window?.rootViewController = feedViewController
     }
 
     func sceneDidDisconnect(_: UIScene) {
