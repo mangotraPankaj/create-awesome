325bac8fbc22718be4ea8e6427fe35338210a861
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-01-03 17:22:40 +0530

    'FeedImageDataLoaderWithFallbackComposite.loadImageData' loads from primary loader first

--- EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
@@ -13,9 +13,13 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
         func cancel() {}
     }
 
-    init(primary _: FeedImageDataLoader, fallback _: FeedImageDataLoader) {}
+    private let primary: FeedImageDataLoader
+    init(primary: FeedImageDataLoader, fallback _: FeedImageDataLoader) {
+        self.primary = primary
+    }
 
-    func loadImageData(from _: URL, completion _: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+    func loadImageData(from url: URL, completion _: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        _ = primary.loadImageData(from: url) { _ in }
         return Task()
     }
 }
@@ -30,7 +34,22 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded url in the primary loader")
     }
 
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in fallback loader")
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL in primary loader")
+    }
+
     // MARK: - Helpers
+
+    private func anyURL() -> URL {
+        return URL(string: "http://a-url.com")!
+    }
 
     private class LoaderSpy: FeedImageDataLoader {
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
