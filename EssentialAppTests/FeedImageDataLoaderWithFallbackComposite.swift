ed3e88f29eda902eec37a8e52a40db91773cddd0
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2022-01-04 11:59:21 +0530

    Move 'FeedImageDataLoaderWithFallbackComposite' into production

--- EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialAppTests/FeedImageDataLoaderWithFallbackComposite.swift
@@ -6,36 +6,8 @@
 //
 
 import EDNLearnMac
+import EssentialApp
 import XCTest
-
-class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
-    private class TaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: FeedImageDataLoaderTask?
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-
-    private let primary: FeedImageDataLoader
-    private let fallback: FeedImageDataLoader
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primary.loadImageData(from: url) { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-            case .failure:
-                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
-            }
-        }
-        return task
-    }
-}
 
 class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
     func test_init_doesNotLoadImageData() {
